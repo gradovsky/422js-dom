830a5dae7dfc91f19cc0d90edc5676c2589455f7
Dima <grad3r@ex.ua>
2019-07-08 13:13:55 +0300

    calculator

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const display = document.querySelector('.calculator .display');
+
+document.querySelectorAll ('.digits button')
+.forEach(digit => digit.addEventListener('click', digitPressed));
+function digitPressed(ev){
+    display.value += ev.target.innerText
+}
+
+
+document
+.querySelectorAll ('.opers button')
+.forEach(oper => oper.addEventListener('click', operPressed));
+function operPressed(ev){
+    display.value += ev.target.innerText
+}
+
+document.querySelector('.equal').addEventListener('click', equalPressed);
+
+function equalPressed() {
+    display.value = eval(display.value);
+}
